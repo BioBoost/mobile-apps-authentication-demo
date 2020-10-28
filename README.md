984c1c3e4cc5a378523266667805b74423e6909f
Nico De Witte <nico.dewitte@outlook.com>
2020-10-28 18:02:36 +0100

    Setup basic layout and add navigation buttons

--- README.md
+++ README.md
@@ -25,3 +25,36 @@ Start the app:
 ```bash
 yarn serve
 ```
+
+## Create Navigation and Main Layout
+
+Add some navigation buttons to the App layout in `App.vue`:
+
+```vue
+<template>
+  <v-app id="authdemo">
+    <v-app-bar
+      app color="red"
+    >
+      <v-spacer></v-spacer>
+      <v-toolbar-items>
+        <v-btn color="white" text>Register</v-btn>
+        <v-btn color="white" text>Login</v-btn>
+        <v-btn color="white" text>Logout</v-btn>
+      </v-toolbar-items>
+    </v-app-bar>
+
+    <v-main id="main">
+      <v-container class="fill-height">
+        <router-view></router-view>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
+```
--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,26 @@
 <template>
-  <v-app>
+  <v-app id="authdemo">
     <v-app-bar
-      app
-      color="primary"
-      dark
+      app color="red"
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
       <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+      <v-toolbar-items>
+        <v-btn color="white" text>Register</v-btn>
+        <v-btn color="white" text>Login</v-btn>
+        <v-btn color="white" text>Logout</v-btn>
+      </v-toolbar-items>
     </v-app-bar>
 
-    <v-main>
-      <HelloWorld/>
+    <v-main id="main">
+      <v-container class="fill-height">
+        <router-view></router-view>
+      </v-container>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
 export default {
-  name: 'App',
-
-  components: {
-    HelloWorld,
-  },
-
-  data: () => ({
-    //
-  }),
+  name: "App",
 };
 </script>
